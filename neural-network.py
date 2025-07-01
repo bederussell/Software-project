7980722c39419dafaa36acbbcebe95ace879c0bf
bederussell <bede.russell@education.nsw.gov.au>
2025-07-01 16:18:44 +1000

    Update neural-network.py
    refined pre-processing code and added code that prepares a padded, tokenized data set for the RNN model.
    
    uses a new training dataset

--- neural-network.py
+++ neural-network.py
@@ -1,33 +1,44 @@
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
-import re
-from collections import Counter
+import pandas as pd
+import tensorflow as tf
 
-text='This is a test set of text data - only used for testing purposes only.'
+text='This is a test set of text data - only used for testing purposes only. Abandonments are not only abbreviating the absentmindedness.'
 stop_words=set(stopwords.words('english'))
 
-def preprocess(data):
-    s=re.sub(r'[\.\?\!\,\:\;\"]', '', data)
-    # Replace punctuation with no space
-    s = re.sub(r"[^\w\s]", '', s)
-    # replace digits with no space
-    s = re.sub(r"\d", '', s)
-    return s
+def tokenize(data):
+    tokenized=[] # list containing lemmatized phrases
+    for item in data:
+        word_list=[]
+        nonstopwords=[]
+        tokens=word_tokenize(item) # tokenize all words in the phrase
+        for word in tokens:
+            if word.strip().lower() not in stop_words: # check if the word is a stop word 
+                nonstopwords.append(word)
+        lemmatizer = WordNetLemmatizer() # initiate lemmatizer object
+        for word in nonstopwords:  # only lemmatize the word if it is not a stop word
+            word=lemmatizer.lemmatize(word).lower() # lemmatize each word
+            word_list.append('{:10}'.format(word))  # pad each word to a certain length (max 12 characters) and add to word list
+        # padding is necessary as the RNN model needs items with a uniform length to iterate through
+        tokenized.append(word_list)
+    return tokenized
 
-def tokenize(text):
-    word_list=[] # list containing lemmatized important words
-    nonstopwords=[]
-    tokens=word_tokenize(text) # tokenize all words in the text
-    for word in tokens:
-        if word.strip().lower() not in stop_words: # check if the word is a stop word 
-            nonstopwords.append(word)
-    lemmatizer = WordNetLemmatizer() # initiate lemmatizer object
-    for word in nonstopwords:  # only lemmatize the word if it is not a stop word
-        word_list.append(lemmatizer.lemmatize(word).lower()) # lemmatize each word and add it to the word list
+df = pd.read_csv("dialogs.txt", sep='\t', header=None, names=["prompt", "response"])
+prompts = df['prompt'].astype(str).tolist()
+responses = df['response'].astype(str).tolist()
 
-    corpus=Counter(word_list) # count the number of times each word appears
-    ordered_corpus = sorted(corpus,key=corpus.get,reverse=True)[:1000] # sort the list into an ordered format from most common to least common words
-    onehot_dict = {w:i+1 for i,w in enumerate(ordered_corpus)} # create an ordered dictionary of most common words that are one-hot encoded
+encoder=tokenize(prompts)
+decoder=tokenize(responses)
 
-    return onehot_dict
+def create_dataset(encoder, decoder):
+    batch_size = 64
+    buffer_size = 1000
+    dataset = tf.data.Dataset.from_tensor_slices(
+        {
+            'encoder_input': encoder,
+            'decoder_input': decoder
+        },
+    )
+    dataset = dataset.shuffle(buffer_size).batch(batch_size)
+    return dataset
